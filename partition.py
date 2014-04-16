84eb729fc024c356cfbd36d48f103116453341ec
Duncan Ralph <dkralph@gmail.com>
2014-04-16 09:01:23 -0700

    Start thinking about making a smart assignment
    I.e. strategies that are better than just comparing genes individually.

--- partition.py
+++ partition.py
@@ -14,7 +14,7 @@ regions = ('v','d','j')
 hmmdir = 'data/hmms'
 
 # set up a dict to access the columns
-hmmer_output_column_list = ['total_E-value', 'total_score', 'total_bias', 'domain_E-value', 'domain_score', 'domain_bias', 'exp', 'N', 'Model']
+hmmer_output_column_list = ['total_evalue', 'total_score', 'total_bias', 'domain_evalue', 'domain_score', 'domain_bias', 'exp', 'N', 'Model']
 hmmer_columns = {}
 for icol in range(len(hmmer_output_column_list)):
     hmmer_columns[hmmer_output_column_list[icol]] = icol
@@ -61,55 +61,87 @@ def build_hmms():
         pressfile.writelines(all_lines)
     call(['../binaries/hmmpress', '-f', outdir + '/all.hmm'])
 
-#build_hmms()
-with opener('r')('/home/dralph/work/recombinator/out.csv') as infile:
-    reader = csv.DictReader(infile)
-    for inline in reader:
-        tmpfile = '/tmp/tmp.fa'
-        with opener('w')(tmpfile) as seqfile:
-            seq_lines = ['> tmp seq file\n', inline['seq'] + '\n']
-            seqfile.writelines(seq_lines)
-#        output = check_output(["../binaries/hmmscan", hmmdir + '/all.hmm', tmpfile])
-        binary = "../binaries/hmmscan"
-        hmm_dbase = hmmdir + '/all.hmm'
-        seq_file = tmpfile
-        logfname = '/tmp/hmmer-log.txt'
-        # first cache the whole output in a file
-        with opener('w')(logfname) as logfile:
-            call([binary, hmm_dbase, seq_file], stdout=logfile)
-        # then filter it with sed to get the main block of scores
-        filter_command = "sed -e '/>>/,/\[ok\]/ d' -e '/^#/ d' -e '/^$/ d' " + logfname + " | grep IGH"
+def find_genes_in_seq(hmm_dbase_file, inline):
+    seqfname = 'tmp/tmp.fa'
+    with opener('w')(seqfname) as seqfile:
+        seqlines = ['> tmp seq file\n', inline['seq'] + '\n']
+        seqfile.writelines(seqlines)
+    binary = "../binaries/hmmscan"
+    # first cache the whole output in a file
+    logfname = '/tmp/hmmer-log.txt'
+#    call([binary, '--tblout ' + logfname, hmm_dbase_file, seq_file])  # wtf? the freaking *manual* says that's an option
+    call([binary, '-o' + logfname, hmm_dbase_file, seqfname])
+    # then filter it with sed to get the main block of scores
+    filter_command = "sed -e '/>>/,/\[ok\]/ d' -e '/^#/ d' -e '/^$/ d' " + logfname + " | grep IGH"
+    output = ''
+    matches = []
+    try:
         output = check_output(filter_command, shell=True)
-        matches = {}
-        for region in regions:
-            matches[region] = []
-        for outline in output.splitlines():
-            gene_name = outline.split()[hmmer_columns['Model']]
-            region = gene_name[3].lower()
-            assert region=='v' or region =='d' or region=='j'
-            matches[region].append(outline)
-        warn_string = ''
-        for region in regions:
-            if len(matches[region]) == 0:
-                print 'na',
-            for match in matches[region]:
-                # make sure almost all the match score is from one domain
-                #print match.split()[hmmer_columns['total_score']]
-                domain_score = float(match.split()[hmmer_columns['domain_score']])
-                total_score = float(match.split()[hmmer_columns['total_score']])
-                simulated_gene = inline[region + '_gene']
-                inferred_gene = unsanitize_name(match.split()[hmmer_columns['Model']])
-                is_correct = simulated_gene == inferred_gene
-                if is_correct:
-                    print ' -',
-                else:
-                    print ' x',
-                if domain_score / total_score < 0.9:
-                    warn_string += '    WARNING: domain / total = %.2f / %.2f = %.2f for %s (%s)' % (domain_score, total_score, domain_score/total_score, simulated_gene, inferred_gene)
-#                    # print more info about that sequence:
-#                    with opener('r')(logfname) as logfile:
-#                        for line in itertools.dropwhile(lambda line: '>> ' + gene_name not in line, logfile.readlines()):
-#                        #for line in itertools.islice(logfile.readlines(), lambda line: '>> ' + gene_name not in line,):
-#                            print line.strip('\n')
-                break  # break after the best match (they should be sorted)
-        print warn_string
+    except:
+        return matches
+    for outline in output.splitlines():
+        gene = unsanitize_name(outline.split()[hmmer_columns['Model']])
+        score = float(outline.split()[hmmer_columns['total_score']])
+        evalue = float(outline.split()[hmmer_columns['total_evalue']])
+        matches.append({'gene': gene, 'score': score, 'evalue': evalue})
+
+    return matches
+    
+
+#build_hmms()
+#with opener('r')('/home/dralph/work/recombinator/out.csv') as infile:
+with opener('r')('out.csv') as infile:
+    reader = csv.DictReader(infile)
+    for region in regions:
+        print '%s region' % region
+        total_ratio = {}
+        total_ratio['ok'] = 0.0
+        total_ratio['no'] = 0.0
+        n_tot = {}
+        n_tot['ok'] = 0
+        n_tot['no'] = 0
+        for inline in reader:
+            matches = find_genes_in_seq(hmmdir + '/' + region + '/all.hmm', inline)
+            simulated_gene = inline[region + '_gene']
+            if len(matches) == 0:
+                print '  no match'
+                continue
+
+            match_first = 'no'  # does the correct gene have the highest score (lowest evalue)?
+            if simulated_gene == matches[0]['gene']:
+                match_first = 'ok'
+            print '  %s' % match_first,
+            for im in range(1, len(matches)):  # look at the next few matches
+                if im > 1:  # only look at the second match a.t.m.
+                    break
+                ratio = matches[0]['evalue'] / matches[im]['evalue']
+                total_ratio[match_first] += ratio
+                n_tot[match_first] += 1
+                match_this = 'no'  # does the correct gene have the highest score (lowest evalue)?
+                if simulated_gene == matches[im]['gene']:
+                    match_this = 'ok'
+                print ' (' + match_this + ',',
+                print '%6.0e)' % ratio,
+            print ''
+        print '   n_ok: %d n_no: %d' % (n_tot['ok'], n_tot['no'])
+        print '   ok: %6.2e no: %6.2e' % (total_ratio['ok'] / n_tot['ok'], total_ratio['no'] / n_tot['no'])
+        sys.exit()
+
+# eg if [0] is 1e-5 and [1] is 1e-4, ratio is < 1
+# or, if first guess is correct, its evalue should be very small, so ratio is very small
+# whereas if they have similar evalues, ratio would be near one
+# if ratio is greater than 1, the list wasn't sorted
+
+# USE A BRT!
+
+# So: if ratio > threshhold, think about using the second match
+#   ... er, but what do I do with it? All I know is that it's *closer*
+#   to being similar, not that it's *right*.
+# turns out like this:
+#     n_ok: 424 n_no: 75
+#     ok: 1.21e-01 no: 2.89e-01
+# so on the ones I get correct, the ratio is *smaller*, a factor two or so.
+
+# oh, right, I guess what I was going to do was if the v id sucked (say, ratio greater than 1.75), identify the j first, chop
+# it out, and see if that improves things. Or, even better, try each v that rates well against combinations of ds and js. We
+# want the whole combo to rate well, after all.
